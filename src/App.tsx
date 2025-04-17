d6ae6c38ba45d7c203cff9918b55ad06514a7608
Your Name <your-email@example.com>
2025-04-17 21:09:55 +0900

    [Feat] Firestore 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import {useEffect,useState} from "react"
-import {app} from "firebaseApp"
+import {app, db} from "firebaseApp"
 import {getAuth, onAuthStateChanged} from "firebase/auth"
 import {ToastContainer} from "react-toastify"
 import Router from "./components/Router"
 import "react-toastify/dist/ReactToastify.css";
 import Loader from "components/Loader"
 function App() {
+  console.log(db);
   const auth=getAuth(app);
   const [init, setInit]=useState<boolean>(false);
   const [isAuthenticated, setIsAuthenticated]=useState<boolean>(
